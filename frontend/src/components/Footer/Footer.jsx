b875b00d627653c1710ffca4d33f9716e7f42461
ShakebShamsi <shakebshamsi@hotmail.com>
2025-07-02 10:36:19 +0530

    Added: AppDownload Section and Footer Component

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import { assets } from '../../assets/assets'
+import './Footer.css'
+
+const Footer = () => {
+   return (
+      <div className='footer' id='footer'>
+         <div className="footer-content">
+            <div className='footer-content-left'>
+               <img className='footer-logo' src={assets.logo} alt="" />
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, incidunt?</p>
+               <div className="footer-social-icons">
+                  <img src={assets.facebook_icon} alt="" />
+                  <img src={assets.twitter_icon} alt="" />
+                  <img src={assets.linkedin_icon} alt="" />
+               </div>
+            </div>
+            <div className='footer-content-center'>
+               <h2>Company</h2>
+               <ul>
+                  <li>Home</li>
+                  <li>About Us</li>
+                  <li>Delivery</li>
+                  <li>Privacy Policy</li>
+               </ul>
+            </div>
+            <div className='footer-content-right'>
+               <h2>Get in Touch</h2>
+               <ul>
+                  <li>+91 88X60-XX099</li>
+                  <li>info.shakeb@gmail.com</li>
+               </ul>
+            </div>
+         </div>
+         <hr />
+         <p className="footer-copyright"> Copyright 2025 &copy; foodKart.com - All Rights Reserved</p>
+      </div >
+   )
+}
+
+export default Footer
