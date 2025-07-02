bc3247a21966564d158635be0dbac0260f8912b2
ShakebShamsi <shakebshamsi@hotmail.com>
2025-07-02 20:53:14 +0530

    FIX: stylings

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -9,25 +9,31 @@ const Footer = () => {
                <img className='footer-logo' src={assets.logo} alt="" />
                <p>FoodKart is your go-to platform for delicious meals delivered right to your door. We partner with trusted kitchens to bring you fresh, affordable, and satisfying food — every single time.</p>
                <div className="footer-social-icons">
-                  <img src={assets.facebook_icon} alt="" />
-                  <img src={assets.twitter_icon} alt="" />
-                  <img src={assets.linkedin_icon} alt="" />
+                  <a href="http://fb.com" target='_blank'>
+                     <img src={assets.facebook_icon} alt="facebook_icon" />
+                  </a>
+                  <a href="http://x.com" target='_blank'>
+                     <img src={assets.twitter_icon} alt="twitter_icon" />
+                  </a>
+                  <a href="http://linkedin.com" target='_blank'>
+                     <img src={assets.linkedin_icon} alt="linkedin_icon" />
+                  </a>
                </div>
             </div>
             <div className='footer-content-center'>
                <h2>Company</h2>
                <ul>
-                  <li>Home</li>
-                  <li>About Us</li>
-                  <li>Delivery</li>
-                  <li>Privacy Policy</li>
+                  <li className="footer-item">Home</li>
+                  <li className="footer-item">About Us</li>
+                  <li className="footer-item">Delivery</li>
+                  <li className="footer-item">Privacy Policy</li>
                </ul>
             </div>
             <div className='footer-content-right'>
                <h2>Get in Touch</h2>
                <ul>
-                  <li>+91 88X60-XX099</li>
-                  <li>info.shakeb@gmail.com</li>
+                  <li className="footer-item">+91 9876543210</li>
+                  <li className="footer-item">info.shakeb@gmail.com</li>
                </ul>
             </div>
          </div>
