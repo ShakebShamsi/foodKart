2863a7ccd299c3237118011d886e53ce92b5e604
ShakebShamsi <shakebshamsi@hotmail.com>
2025-07-02 20:31:13 +0530

    FEAT: CTA added to HeroSection,  styling

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -15,9 +15,22 @@ const Navbar = ({ setShowLogin }) => {
          </Link>
          <ul className="navbar-menu">
             <Link to='/' onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</Link>
-            <a href='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</a>
-            <a href='#app-download' onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>Mobile-App</a>
-            <a href='#footer' onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>Contact Us</a>
+            <a
+               href='#explore-menu'
+               onClick={() => setMenu("menu")}
+               className={menu === "menu" ? "active" : ""}>
+               Menu
+            </a>
+            <a href='#app-download'
+               onClick={() => setMenu("mobile-app")}
+               className={menu === "mobile-app" ? "active" : ""}>
+               Mobile-App
+            </a>
+            <a href='#footer'
+               onClick={() => setMenu("contact-us")}
+               className={menu === "contact-us" ? "active" : ""}>
+               Contact Us
+            </a>
          </ul>
          <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
