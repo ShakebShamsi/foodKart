1e65ed004779f94d8dbb918c88e93e4230fb9b35
ShakebShamsi <shakebshamsi@hotmail.com>
2025-07-03 13:38:20 +0530

    FIX: delivery cost

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -40,12 +40,12 @@ const PlaceOrder = () => {
                   <hr />
                   <div className="cart-total-details">
                      <p>Delivery Fee</p>
-                     <p>₹{getTotalCartAmount() === 0 ? 0 : 2}</p>
+                     <p>₹{getTotalCartAmount() === 0 ? 0 : 49}</p>
                   </div>
                   <hr />
                   <div className="cart-total-details">
                      <b>Total</b>
-                     <b>₹{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
+                     <b>₹{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 49}</b>
                   </div>
                   <button>PROCEED TO PAYMENT</button>
                </div>
