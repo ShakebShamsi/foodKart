f933b78fc44d2984b4599269de217c74c2c3ddd4
ShakebShamsi <shakebshamsi@hotmail.com>
2025-07-03 13:28:13 +0530

    FIX: currency

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -29,7 +29,7 @@ const FoodItem = ({ id, name, price, description, image }) => {
                {description}
             </p>
             <p className="food-item-price">
-               ${price}
+               ₹{price}
             </p>
          </div>
       </div>
