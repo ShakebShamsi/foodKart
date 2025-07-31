32a3d8601167f2022fed6c7485fca118dc68bfaa
ShakebShamsi <shakebshamsi@hotmail.com>
2025-07-31 12:54:21 +0530

    indentation

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -6,7 +6,7 @@ import { toast } from 'react-toastify';
 const List = () => {
    const url = 'http://localhost:4000'
    const [list, setList] = useState([])
-   
+
    const fetchList = async () => {
       try {
          const response = await axios.get(`${url}/api/food/list`);
@@ -21,9 +21,9 @@ const List = () => {
       }
    }
 
-   const removeFood =async (foodId) => {
+   const removeFood = async (foodId) => {
       const response = await axios.post(`${url}/api/food/remove`, { id: foodId });
-      
+
    }
 
    useEffect(() => {
@@ -31,7 +31,7 @@ const List = () => {
    }, [])
 
    return (
-     <div className='list add flex-col'>
+      <div className='list add flex-col'>
          <p>All Foods List</p>
          <div className="list-table">
             <div className="list-table-format title">
@@ -42,21 +42,21 @@ const List = () => {
                <b>Action</b>
             </div>
             {Array.isArray(list) &&
-            list.map((item, index) => {
-               return (
-                  <div key={index} className="list-table-format">
-                     <img src={item.image} alt={item.name} style={{ width: "60px" }} />
-                     <p>{item.name}</p>
-                     <p>{item.category}</p>
-                     <p>₹{item.price}</p>
-                     <p onClick={()=>removeFood(item._id)} className='cursor'>X</p>
-                  </div>
-               )
-            })}
+               list.map((item, index) => {
+                  return (
+                     <div key={index} className="list-table-format">
+                        <img src={item.image} alt={item.name} style={{ width: "60px" }} />
+                        <p>{item.name}</p>
+                        <p>{item.category}</p>
+                        <p>₹{item.price}</p>
+                        <p onClick={() => removeFood(item._id)} className='cursor'>X</p>
+                     </div>
+                  )
+               })}
 
          </div>
-    </div>
-  )
+      </div>
+   )
 }
 
 export default List
