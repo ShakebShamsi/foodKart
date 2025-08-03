ba21dc4142694841bd3a3f8d268ff9c8e9b6cc39
ShakebShamsi <shakebshamsi@hotmail.com>
2025-08-03 12:16:37 +0530

    final

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,10 +5,8 @@ const Header = () => {
          <div className="header-contents">
             <h2>Order your favourite foods here!</h2>
             <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience—one delicious meal at a time.</p>
-            <button onClick={() => {
-               document.getElementById("explore-menu")?.scrollIntoView();
-            }}>
-               View Menu
+            <button>
+               Scroll Down!
             </button>
          </div>
       </div>
