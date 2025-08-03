0bd19aad67598d9dadc98cf3b8ae835566df57fe
ShakebShamsi <shakebshamsi@hotmail.com>
2025-08-03 08:33:13 +0530

    order page created

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -98,5 +98,16 @@ const userOrders = async  (req, res) => {
    }
 }
 
+const listOrders = async (req, res) => {
+   try {
+      const orders = await orderModel.find({})
+      res.json({success:true, data:orders})
+   } catch (error) {
+      console.log(error);
+      res.json({success:false, message:"Error"})
+      
+   }
+}
 
-export { placeOrder, verifyOrder, userOrders };
+
+export { placeOrder, verifyOrder, userOrders, listOrders };
